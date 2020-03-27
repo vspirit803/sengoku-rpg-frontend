033874c2e03ae340fe7e302436584e05dda5070d
vspirit803 <lx546488173@gmail.com>
2020-03-27 17:57:57 +0800

    feat: display version

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,25 @@
 <template>
     <div class="about">
         <h1>This is an about page</h1>
+        <v-chip>存档版本:{{ version }}</v-chip>
     </div>
 </template>
+<script lang="ts">
+import { createComponent, inject } from '@vue/composition-api';
+import { Game } from '@src/Game';
+
+export default createComponent({
+    name: 'About',
+    props: {},
+    setup() {
+        if (!inject('game')) {
+            throw new Error('没有获取到Game实例');
+        }
+        const game = inject('game') as Game;
+        const version = game.generateSave().version;
+        return {
+            version,
+        };
+    },
+});
+</script>
